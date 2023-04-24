0db5a5c79e1283e2cb9e926a655653442a7151ad
ecourse <smkang@codenuri.co.kr>
2023-04-24 10:19:02 +0900

    Add files via upload

---
+++ DAY1/01_static_assert2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <type_traits> 
+
+#pragma pack(1)	// 구조체 멤버 정렬을 1byte 단위로
+struct PACKET
+{
+	char cmd;
+	int  data;
+};
+static_assert(sizeof(PACKET) == sizeof(char) + sizeof(int),
+			"error, unexpected padding");
+
+
+/*
+int main()
+{
+	std::cout << sizeof(PACKET) << std::endl; // ?
+}
+*/
+
+template<typename T> void object_set_zero(T* p)
+{
+	// std::is_polymorphic<T>::value : T 타입이 가상함수가 있으면
+	//									true 
+
+	static_assert(!std::is_polymorphic<T>::value,
+		
+		"error, T has virtual function");
+
+	memset(p, 0, sizeof(T)); // 안전한 코드 일까요 ?
+}
+
+class A
+{
+	int data;
+public:
+};
+int main()
+{
+	A a;
+	object_set_zero(&a);
+
+}
