6670c8fdf5d56d8429c30ef832fe0ee6127421fa
ecourse <smkang@codenuri.co.kr>
2023-04-24 11:36:59 +0900

    Add files via upload

--- DAY1/05_range_for3.cpp
+++ DAY1/05_range_for3.cpp
@@ -8,6 +8,9 @@ struct Point3D
 // 사용자 정의 타입의 객체를 range-for 에 넣으려면 
 // 아래 같이 begin, end가 있으면 됩니다.
 // 단, 모든 멤버(요소)가 동일타입의 연속된 메모리인 경우만
+// => 위 코드도 x, y, z 가 연속된 메모리라는 보장은 없습니다.
+//    따라서 아래 처럼 만드는 것은 좋지는 않습니다.!
+//    멤버가 배열이었다면 ok!!
 int* begin(Point3D& p) { return &(p.x);}
 int* end(Point3D& p)   { return &(p.z) + 1; }
 
