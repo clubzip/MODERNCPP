57b9ca836b9e5556d2f8b3fed5d4290c7d5b629a
ecourse <smkang@codenuri.co.kr>
2023-04-24 11:49:41 +0900

    Add files via upload

--- DAY1/05_range_for4.cpp
+++ DAY1/05_range_for4.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <vector>
 
-template<typename T>
-class reverse_view
+template<typename T> class reverse_view
 {
 	T& c;
 public:
 	reverse_view(T& c) : c(c) {}
-
-	auto begin() { return ? ; }
-	auto end()   { return ? ; }
+	auto begin() { return c.rbegin() ; }
+	auto end()   { return c.rend() ; }
 };
-
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 };
 
-	for (auto e : v) // 앞쪽 부터 열거
+//	reverse_view<std::vector<int>> rv(v);
+	reverse_view rv(v); // C++17 부터는 템플릿인자 생략가능
+
+//	for (auto e : v) // 앞쪽 부터 열거
+//	for (auto e : rv) 
+	for (auto e : reverse_view(v) ) // 임시객체로 전달
 	{
 		std::cout << e << ", ";
 	}
