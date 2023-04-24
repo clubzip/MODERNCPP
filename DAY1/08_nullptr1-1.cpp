9aa6b3a02268ba57cf0499300d5f3a166f1676ef
ecourse <smkang@codenuri.co.kr>
2023-04-24 13:32:38 +0900

    Add files via upload

--- DAY1/08_nullptr1-1.cpp
+++ DAY1/08_nullptr1-1.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
-template<typename T>
-void forward_foo(T arg)
-{
-	foo(arg);
-}
-
 void foo(int* p) {}
+
+template<typename T>
+void forward_foo(T arg) // int arg = 0 을 추론
+{	
+	foo(arg);	// arg가 int 타입이므로 error
+}
 
 int main()
 {
 	int n = 0;
-	foo(0);
-	foo(n);
+	foo(0);	// ok
+//	foo(n);	// error. 리터럴 0만 포인터로 변환되고
+			//		  0으로 초기화된 정수는 포인터 변환 안됨
+	foo(0);         // ok. 0을 함수 에 직접 전달
+//	forward_foo(0); // ??  0을 전달해 달라고 요청..
+					// error.
+	forward_foo(nullptr); // ok
 }
