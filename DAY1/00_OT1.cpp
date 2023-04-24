a312a62e6e7016951e3c682beef69f919e1a640c
ecourse <smkang@codenuri.co.kr>
2023-04-24 09:44:35 +0900

    Add files via upload

---
+++ DAY1/00_OT1.cpp
@@ -0,0 +1,16 @@
+
+// github.com/codenuri/moderncpp
+
+// 1. 프로젝트 메뉴 선택후
+// 2. DAY1 속성 메뉴 선택
+// 3. 플랫폼 도구 집합 버전 변경
+//    Windows SDK 버전 변경
+
+// control + F5
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "modern c++" << std::endl;
+}
