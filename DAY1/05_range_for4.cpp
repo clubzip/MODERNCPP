1bd64f76221f529753ce0577dbf7acb50bbcca80
ecourse <smkang@codenuri.co.kr>
2023-04-24 11:43:24 +0900

    Add files via upload

--- DAY1/05_range_for4.cpp
+++ DAY1/05_range_for4.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <vector>
 
+template<typename T>
+class reverse_view
+{
+	T& c;
+public:
+	reverse_view(T& c) : c(c) {}
+
+	auto begin() { return ? ; }
+	auto end()   { return ? ; }
+};
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 };
 
-	for (auto e : v)
+	for (auto e : v) // 앞쪽 부터 열거
 	{
 		std::cout << e << ", ";
 	}
