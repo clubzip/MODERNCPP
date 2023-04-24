588875a9957f3e311c6fc1dad2ea91cab31709e8
ecourse <smkang@codenuri.co.kr>
2023-04-24 13:23:28 +0900

    Add files via upload

---
+++ DAY1/08_nullptr.cpp
@@ -0,0 +1,15 @@
+// 8_nullptr - 29page
+
+int main()
+{
+	// C++98 시절
+	int  n1 = 0; // 정수 초기화도 0
+	int* p1 = 0; // 포인터 초기화도 0
+
+	// C++11 부터는 되도록이면 포인터는 nullptr 사용하세요
+	int  n2 = 0; 
+	int* p2 = nullptr; // ok.. good  
+
+//	int n3 = nullptr; // error.
+
+}
