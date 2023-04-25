8c06abcddef1785f945e55849129828a729633dd
ecourse <smkang@codenuri.co.kr>
2023-04-25 11:27:56 +0900

    Add files via upload

--- DAY2/06_constexpr5.cpp
+++ DAY2/06_constexpr5.cpp
@@ -1,5 +1,5 @@
 // 17_static_if - 51 page
-
+#include <type_traits>
 template<typename T>
 void foo(T a)
 {
@@ -13,10 +13,15 @@ void foo(T a)
 	//				  조건이 false 라면 인스턴스화에 포함안됨
 	//				  C++17 에서 도입된 문법
 	if constexpr (false)
-		*a = 10; // 이 코드는 에러일까요 ??
+		*a = 10; // 에러 아님.
 
+	// 실전에서는 아래 처럼 사용합니다 - 목요일날자세히
+	if constexpr (std::is_pointer<T>::value)
+	{
+		// 이부분은 T가 포인터인 경우만 인스턴스화에 포함되고
+		// T가 포인터가 아니면 컴파일 시에 함수 생성시 제거됩니다.
+	}
 }
-
 int main()
 {
 	foo(0); 
