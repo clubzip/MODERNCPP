0f7628798a1b657166854dc5cb4d0632d097b145
ecourse <smkang@codenuri.co.kr>
2023-04-25 11:47:49 +0900

    Add files via upload

--- DAY2/10_타입추론1-1.cpp
+++ DAY2/10_타입추론1-1.cpp
@@ -3,7 +3,14 @@
 
 template<typename T> void foo(T a)
 {
+	// T의 타입을 확인하는 방법
+	// 1. RTTI 기술 사용
+	// => 사용하지 마세요
+	// => C++ RTTI 는 const, volatile, reference 를 제거한
+	//    타입의 이름이 출력됩니다.
+	std::cout << typeid(T).name() << std::endl;
 }
+
 int main()
 {
 	// 타입을 명시적으로 전달
