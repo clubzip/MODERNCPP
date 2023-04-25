12b1f421ca590a37d6bb876530a2c58ea37ff628
ecourse <smkang@codenuri.co.kr>
2023-04-25 16:50:02 +0900

    Add files via upload

--- DAY2/12_rvalue3.cpp
+++ DAY2/12_rvalue3.cpp
@@ -28,12 +28,18 @@ int main()
 {
 	int n = 10;
 
-	CHECK_VALUE_CATEGORY(n);
-	CHECK_VALUE_CATEGORY(3);
-	CHECK_VALUE_CATEGORY(++n);
-	CHECK_VALUE_CATEGORY(n++);
-	CHECK_VALUE_CATEGORY(n + n);
-	CHECK_VALUE_CATEGORY(n=20);
+//	decltype(n); // 규칙1. 이므로 int 타입 입니다.rvalue 로 판단하게 됩니다.
+//	decltype((n)); // () 때문에 규칙 2. 사용. 즉 =의 왼쪽에 올수 있는가로 판단
+
+	CHECK_VALUE_CATEGORY(n);	// lvalue
+	CHECK_VALUE_CATEGORY(3);	// rvaue
+	CHECK_VALUE_CATEGORY(++n);	// lvalue
+	CHECK_VALUE_CATEGORY(n++);	// rvalue
+	CHECK_VALUE_CATEGORY(n+n);	// rvalue
+	CHECK_VALUE_CATEGORY(n=20);	// lvalue
 
 	CHECK_VALUE_CATEGORY("hello"); // 핵심!!. 놀라운 결과
+					// 문자열 리터럴은 lvalue 로 구분 됩니다.
+					// "hello" 가 데이타 메모리를 할당(상수메모리)
+					// 해서 놓이기 때문에 그렇게 구분했습니다.
 }
