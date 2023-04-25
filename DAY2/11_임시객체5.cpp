849b446a06708bc160a9a5e5d5899096a5ca75c0
ecourse <smkang@codenuri.co.kr>
2023-04-25 15:52:10 +0900

    Add files via upload

--- DAY2/11_임시객체5.cpp
+++ DAY2/11_임시객체5.cpp
@@ -17,8 +17,12 @@ int main()
 	std::cout << static_cast<Base&>(d).value << std::endl; // 10
 	std::cout << static_cast<Base>(d).value << std::endl; // 10
 
-	static_cast<Base&>(d).value = 10; // ok
-	static_cast<Base>(d).value = 10; // error. 
+	static_cast<Base&>(d).value = 30; // ok
+//	static_cast<Base>(d).value = 30; // error. 
 								// 값 캐스팅은 임시객체를 생성한것
-}
 
+	std::cout << d.value << std::endl; // 20
+	std::cout << static_cast<Base&>(d).value << std::endl; // 30
+}
+// Base 안에 "복사 생성자" "소멸자" 만들어서 로깅해 보시면
+// 임시객체 생성되는 결과 확인 가능합니다.
