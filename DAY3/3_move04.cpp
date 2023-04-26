f6ca2343463d2e65e0cb59134cf0a7e4192001d0
ecourse <smkang@codenuri.co.kr>
2023-04-26 10:27:11 +0900

    Add files via upload

--- DAY3/3_move04.cpp
+++ DAY3/3_move04.cpp
@@ -20,7 +20,18 @@ public:
 		name = new char[strlen(c.name) + 1];
 		strcpy_s(name, strlen(c.name) + 1, c.name);
 	}
+	// 임시객체를 위한 복사 생성자를 
+	// 추가로 만들자는 것!!
+	// 임시객체는 rvalue 이므로 인자를 "Cat&&"
+	Cat(Cat&& c) : age(c.age), name(c.name)
+	{
+		// 아래 코드를 수행하기 위해서
+		// 임시객체도 상수성없이 가리킬수 있어야 합니다.
+		c.name = nullptr; 
+	}
 };
+
+
 
 Cat foo()
 {
