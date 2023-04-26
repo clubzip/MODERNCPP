0a8c196a404714ad604aa4702d349f110d0873fb
ecourse <smkang@codenuri.co.kr>
2023-04-26 11:36:09 +0900

    Add files via upload

--- DAY3/3_move08.cpp
+++ DAY3/3_move08.cpp
@@ -15,12 +15,22 @@
 
 // => 컴파일러에게 move 계열 함수의 디폴트 버전을 요청하려면 
 //    "=default"사용
+
+// 규칙 3. 사용자가 move 계열만 제공한 경우
+// => 컴파일러는 복사 계열을 "삭제" 합니다.
+// => 이동이 필요 할때 복사를 사용할수 있지만
+//    복사가 필요 할때 이동을 사용할수는 없으므로
+//    복사 생성자가 필요한 코드를 사용하는 경우 컴파일 에러.
+
+// => 이 경우도 복사 계열을 "= default"로 요청 가능합니다.
+
 struct Object
 {
 	std::string name;
 
 	Object(const std::string& n) : name(n) {} // 생성자
 
+	/*
 	Object(const Object& obj) : name(obj.name)
 	{
 		std::cout << "사용자가 만든 복사 생성자" << std::endl;
@@ -30,8 +40,13 @@ struct Object
 	Object(Object&&) = default;
 	Object& operator=(const Object&) = default;
 	Object& operator=(Object&&) = default;
-};
+	*/
 
+	Object(Object&& obj) : name(std::move(obj.name))
+	{
+		std::cout << "사용자가 만든 이동 생성자" << std::endl;
+	}
+};
 int main()
 {
 	Object o1("obj1 to be or not to be");
