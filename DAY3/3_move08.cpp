bcc4210d853afa6626ca3083d6c87481a7de3e0e
ecourse <smkang@codenuri.co.kr>
2023-04-26 11:28:29 +0900

    Add files via upload

--- DAY3/3_move08.cpp
+++ DAY3/3_move08.cpp
@@ -9,13 +9,23 @@
 // 자동생성된 복사 계열 함수 : 모든 멤버를 복사 해준다.
 // 자동생성된 이동 계열 함수 : 모든 멤버를 이동(std::move()) 해준다.
 
+// 규칙 2. 사용자가 복사 계열만 제공할때
+// => 컴파일러는 move 계열 함수를 자동생성하지않음.
+// => std::move() 사용시 사용자가 만든 복사 계열 함수를 사용하게됨.
+
+// => 컴파일러에게 move 계열 함수의 디폴트 버전을 요청하려면 
+//    "=default"사용
 struct Object
 {
 	std::string name;
 
 	Object(const std::string& n) : name(n) {} // 생성자
-};
 
+	Object(const Object& obj) : name(obj.name)
+	{
+		std::cout << "사용자가 만든 복사 생성자" << std::endl;
+	}
+};
 
 int main()
 {
@@ -23,8 +33,8 @@ int main()
 	Object o2("obj2 to be or not to be");
 
 	Object o3 = o1;
-	Object o4 = std::move(o1);
+	Object o4 = std::move(o2);
 
 	std::cout << o1.name << std::endl;
-	std::cout << o2.name << std::endl;
+	std::cout << o2.name << std::endl; // ""
 }
