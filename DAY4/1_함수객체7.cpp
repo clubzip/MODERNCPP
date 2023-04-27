9271064e1e98edbaecf371f3e026eb14ee67bff2
ecourse <smkang@codenuri.co.kr>
2023-04-27 11:27:49 +0900

    Add files via upload

---
+++ DAY4/1_함수객체7.cpp
@@ -0,0 +1,20 @@
+// 1_함수객체7.cpp
+// cmp1, cmp2, Less, Greater 복사해오세요
+
+template<typename T>
+void foo(T f)
+{
+	bool b = f(1, 2); // A
+}
+int main()
+{
+	Less    f1;
+	Greater f2;
+	foo(cmp1);
+	foo(cmp2);
+	foo(f1);
+	foo(f2);
+}
+// 1. foo 함수는 몇개 생성될까요 ?
+// 2. 각 foo 함수에서 "A" 부분의 인라인 치환에 대해서 생각해 보세요
+// 3. foo와 main 을 제외하고 기계어 코드로 생성된 함수는 무엇이 있을까요?
