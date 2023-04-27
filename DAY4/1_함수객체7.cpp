7a005b95a90c9605c90e524e1bdb546a498ade83
ecourse <smkang@codenuri.co.kr>
2023-04-27 11:29:06 +0900

    Add files via upload

--- DAY4/1_함수객체7.cpp
+++ DAY4/1_함수객체7.cpp
@@ -1,5 +1,9 @@
 // 1_함수객체7.cpp
 // cmp1, cmp2, Less, Greater 복사해오세요
+inline bool cmp1(int a, int b) { return a < b; }
+inline bool cmp2(int a, int b) { return a > b; }
+struct Less { inline bool operator()(int a, int b) { return a < b; } };
+struct Greater { inline bool operator()(int a, int b) { return a > b; } };
 
 template<typename T>
 void foo(T f)
