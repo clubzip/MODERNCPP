434bc3d360ac019063516b43a74fd943c5d32f6b
ecourse <smkang@codenuri.co.kr>
2023-04-27 10:35:47 +0900

    Add files via upload

---
+++ DAY4/1_함수객체4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm> 
+
+// 비교 정책을 인자로 전달 받는 sort
+void Sort(int* x, int sz, bool(*cmp)(int, int) )
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = i + 1; j < sz; j++)
+		{
+//			if (x[i] > x[j])				
+			if ( cmp(x[i], x[j]) == true )
+				std::swap(x[i], x[j]);
+		}
+	}
+}
+//------------------------
+bool cmp1(int a, int b) { return a > b; }
+bool cmp2(int a, int b) { return a < b; }
+
+int main()
+{
+	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
+	Sort(x, 10, &cmp1); 
+}
+
