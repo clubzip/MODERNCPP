f434fae260d2dd1b5f0481b27c96c8771e49c409
ecourse <smkang@codenuri.co.kr>
2023-04-27 10:40:50 +0900

    Add files via upload

--- DAY4/1_함수객체4.cpp
+++ DAY4/1_함수객체4.cpp
@@ -2,21 +2,24 @@
 #include <algorithm> 
 
 // 비교 정책을 인자로 전달 받는 sort
+// C언어의 qsort()가 아래와 유사하게 함수 포인터 전달 받습니다.
+// => 데이타 량이 많으면 성능이 느려 집니다.
 void Sort(int* x, int sz, bool(*cmp)(int, int) )
 {
 	for (int i = 0; i < sz - 1; i++)
 	{
 		for (int j = i + 1; j < sz; j++)
 		{
-//			if (x[i] > x[j])				
-			if ( cmp(x[i], x[j]) == true )
+//			if (x[i] > x[j])		// 1
+
+			if ( cmp(x[i], x[j]) == true ) // 2
 				std::swap(x[i], x[j]);
 		}
 	}
 }
 //------------------------
-bool cmp1(int a, int b) { return a > b; }
-bool cmp2(int a, int b) { return a < b; }
+inline bool cmp1(int a, int b) { return a > b; }
+inline bool cmp2(int a, int b) { return a < b; }
 
 int main()
 {
