6c4a357af779e14323c61ae268c54075b2a8dbe7
ecourse <smkang@codenuri.co.kr>
2023-04-27 10:16:07 +0900

    Add files via upload

--- DAY4/1_함수객체2-1.cpp
+++ DAY4/1_함수객체2-1.cpp
@@ -10,7 +10,6 @@ struct IsMod
 {
 	int value;
 	IsMod(int n) : value(n) {}
-
 	bool operator()(int n) const { 	return n % value == 0;	}
 };
 int main()
@@ -21,9 +20,17 @@ int main()
 	IsMod f(k); // f는 단항함수 처럼 사용가능한데. k값을 보관하고 있습니다.
 	bool b = f(6);// 6 % k 를 조사
 
+	// f : operator() 함수 기능 + main 지역변수 k값
+
 	auto p1 = std::find_if(v.begin(), v.end(), f) ;
-
+									// "단항함수 f + k값"
+								    // 을 같이 전달하는 의미
+	auto p2 = std::find_if(v.begin(), v.end(), IsMod{k});
 }
+// 클로져(Closure)
+// => 자신이 포함된 문맥의 지역변수를 캡쳐할수 있는 일급객체(함수)
 
+// C++에서 일반 함수는 클로져가 될수 없지만
+//        함수객체는 클로져가 될수 있다.
 
 
