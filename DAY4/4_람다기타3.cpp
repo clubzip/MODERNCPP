858f9df16dd7c65f068ec607bec4f85c358c1338
ecourse <smkang@codenuri.co.kr>
2023-04-27 15:48:40 +0900

    Add files via upload

--- DAY4/4_람다기타3.cpp
+++ DAY4/4_람다기타3.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
+
 int main()
 {
-	auto swap = [](int& a, int& b) { int tmp = std::move(a); 
+	auto swap = [](auto& a, auto& b) { auto tmp = std::move(a); 
 										   a = std::move(b); 
 										   b = std::move(tmp); };
 
 	int n = 10;
 	double d = 3.4;
+
+	swap(n, d); //
+
+	// C++14 generic 람다 : 인자가 서로 다른 템플릿!!!
+	// C++20 template 람다가 도입되었습니다.
+
+	auto swap2 = []<typename T>(T& a, T& b) { T tmp = std::move(a);
+										a = std::move(b);
+										b = std::move(tmp); };
+	swap2(n, d); // error
 }
