8d10cfb354839e864b6308d74ea5de60793fe4d6
ecourse <smkang@codenuri.co.kr>
2023-04-27 14:45:08 +0900

    Add files via upload

--- DAY4/3_람다_지역변수캡쳐2.cpp
+++ DAY4/3_람다_지역변수캡쳐2.cpp
@@ -4,8 +4,12 @@ int main()
 {
 	int v1 = 10, v2 = 10;
 
-	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
+//	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
 	
+	// mutable 람다 표현식 : operator() 연산자를 const 함수로 하지말라는것
+	auto f1 = [v1, v2](int a) mutable { v1 = 100; return a + v1 + v2; };
+
+
 	// 위 f1 의  정확한 원리를 생각해 봅시다. => 꼭 이해해 두세요
 	class CompilerGeneratedName
 	{
@@ -14,9 +18,10 @@ int main()
 	public:
 		CompilerGeneratedName(int a, int b) : v1(a), v2(b) {}
 
-		inline auto operator()(int a) const
+		inline auto operator()(int a) // const
 		{
-			v1 = 100; 
+			v1 = 100;				// () 연산자 함수가 상수함수라면
+									// error 입니다.
 			return a + v1 + v2;
 		}
 	};
