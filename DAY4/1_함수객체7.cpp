75a58a5b98c6cf592eb59e003f7f520a7b58efe8
ecourse <smkang@codenuri.co.kr>
2023-04-27 11:32:12 +0900

    Add files via upload

--- DAY4/1_함수객체7.cpp
+++ DAY4/1_함수객체7.cpp
@@ -14,11 +14,13 @@ int main()
 {
 	Less    f1;
 	Greater f2;
-	foo(cmp1);
-	foo(cmp2);
-	foo(f1);
-	foo(f2);
+	foo(cmp1); // foo(bool(*)(int, int)) => 치환 안됨
+	foo(cmp2); // foo(bool(*)(int, int)) =>
+	foo(f1);   // foo(Less)    => 치환 가능
+	foo(f2);   // foo(Greater) => 치환 가능
 }
-// 1. foo 함수는 몇개 생성될까요 ?
+// 1. foo 함수는 몇개 생성될까요 ? => 3개
 // 2. 각 foo 함수에서 "A" 부분의 인라인 치환에 대해서 생각해 보세요
 // 3. foo와 main 을 제외하고 기계어 코드로 생성된 함수는 무엇이 있을까요?
+
+// godbolt.org 에.. 함수객체 3번 복사해 넣으세요
