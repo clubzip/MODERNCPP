307606867ce7f4e410df56c07aa26c4d3639754f
ecourse <smkang@codenuri.co.kr>
2023-04-28 11:07:33 +0900

    Add files via upload

---
+++ DAY5/2_type_traits5.cpp
@@ -0,0 +1,25 @@
+// traits5.cpp - 1번 복사해오세요
+#include <iostream>
+#include <type_traits>
+
+template<typename T> void printv(const T& v)
+{
+	if ( std::is_pointer_v<T> )
+		std::cout << v << " : " << *v << std::endl;
+	else
+		std::cout << v << std::endl;
+}
+
+
+
+
+int main()
+{
+	int n = 10;
+	double d = 3.4;
+
+	printv(n);
+	printv(d);
+	printv(&n);
+}
+
