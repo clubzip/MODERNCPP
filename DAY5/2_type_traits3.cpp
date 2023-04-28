a0b2ef780db746345580c899ca62f50146c4c8aa
ecourse <smkang@codenuri.co.kr>
2023-04-28 10:19:13 +0900

    Add files via upload

---
+++ DAY5/2_type_traits3.cpp
@@ -0,0 +1,38 @@
+// 4_type_traits3 - 210 page
+#include <iostream>
+#include <type_traits>
+
+// traits 의 종류
+// 1. 타입질의 : is_xxx<T>::value
+// 2. 변형타입얻기 : xxx<T>::type
+
+// 1. primary template 을 만들고 "using type = T" 넣으세요
+
+template<typename T> struct remove_pointer
+{
+	using type = T;
+};
+
+// 2. 부분특수화를 사용해서 원하는 타입을 얻을수 있도록 
+//    타입을 분리 합니다.
+//    "int*" => "int" 와 "*" 로 분리
+template<typename T> struct remove_pointer<T*>
+{
+	using type = T;
+};
+
+int main()
+{
+	remove_pointer<int*>::type n2; 
+
+	std::cout << typeid(n2).name() << std::endl; // int
+}
+
+
+
+
+
+
+template<typename T> void foo(T a)
+{
+}
