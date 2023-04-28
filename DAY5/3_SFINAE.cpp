3c90d772c8b83e07a552ec025d57ebf10126ad67
ecourse <smkang@codenuri.co.kr>
2023-04-28 11:23:32 +0900

    Add files via upload

--- DAY5/3_SFINAE.cpp
+++ DAY5/3_SFINAE.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 
 //template<typename T> 
-//void foo(T a)   { std::cout << "T" << std::endl; }
+// void foo(T a)   { std::cout << "T" << std::endl; }
 
 // void foo(int a) { std::cout << "int" << std::endl; }
 
-//void foo(...)   { std::cout << "..." << std::endl; }
+// void foo(...)   { std::cout << "..." << std::endl; }
 
 int main()
 {
@@ -14,4 +14,8 @@ int main()
 			// 2. T.   템플릿으로 foo(int) 생성
 			// 3. ...
 			// 4. 함수가 없다고 에러
+
+	// 동일 이름의 함수가 여러개 있을때 찾는 원리
+	// => overloading resolution
+	// => 규칙이 상당히 복잡 합니다.
 }
