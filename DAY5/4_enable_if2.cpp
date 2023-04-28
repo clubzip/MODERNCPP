4dc2e9a13bc4e696c87aff4608e7b5150749a2ed
ecourse <smkang@codenuri.co.kr>
2023-04-28 13:06:30 +0900

    Add files via upload

--- DAY5/4_enable_if2.cpp
+++ DAY5/4_enable_if2.cpp
@@ -9,15 +9,14 @@ template<typename T> T gcd(T a, T b)
 
 	return 0; // 구현생략..
 }
-
 double gcd(double a, double b)
 {
 	std::cout << "double" << std::endl;
 	return 0;
 }
-
 int main()
 {
 	gcd(6, 9);
 	gcd(3.1,  5.4);
+	gcd(3.1f, 5.4f);
 }
