f98e6f3d7d739650fd6b33cef5fbd8c9fbaf246a
ecourse <smkang@codenuri.co.kr>
2023-04-28 13:05:00 +0900

    Add files via upload

---
+++ DAY5/4_enable_if2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <type_traits>
+
+// 정수의 최대 공약수 구하는 함수
+template<typename T> T gcd(T a, T b)
+{
+	static_assert(std::is_integral_v<T>,
+				"error T is not integer");
+
+	return 0; // 구현생략..
+}
+
+double gcd(double a, double b)
+{
+	std::cout << "double" << std::endl;
+	return 0;
+}
+
+int main()
+{
+	gcd(6, 9);
+	gcd(3.1,  5.4);
+}
