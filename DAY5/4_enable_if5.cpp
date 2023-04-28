7042de1543fa70a06f589a967ee6e70291e92c38
ecourse <smkang@codenuri.co.kr>
2023-04-28 13:34:48 +0900

    Add files via upload

--- DAY5/4_enable_if5.cpp
+++ DAY5/4_enable_if5.cpp
@@ -4,18 +4,14 @@
 // 리턴 타입 자리에 "std::enable_if_t<조건, 반환타입>" 표기해보세요
 
 template<typename T>
-
-void 
-
+std::enable_if_t< std::is_pointer_v<T> >
 printv(const T& v)
 {
 	std::cout << v << " : " << *v << std::endl;
 }
 
 template<typename T>
-
-void 
-
+std::enable_if_t< !std::is_pointer_v<T> >
 printv(const T& v)
 {
 	std::cout << v << std::endl;
