6bfe0f0c758e5cfdf971fa329c129e2c6ccdaa18
ecourse <smkang@codenuri.co.kr>
2023-04-28 10:49:05 +0900

    Add files via upload

--- DAY5/2_type_traits7.cpp
+++ DAY5/2_type_traits7.cpp
@@ -12,9 +12,19 @@ public:
 // T&  : lvalue 받겠다는 것
 // T&& : lvalue, rvalue 를 모두 받겠다는것
 template<typename T>
-T&& mymove(T&& obj)
+std::remove_reference_t<T>&&  mymove(T&& obj)
 {
-	return static_cast<T&&>(obj);
+	// T&& 를 인자로 사용하면
+	// lvalue(o3) 을 보냈을때 T=Object& 입니다.
+	// 따라서 아래 코드는 static_cast<Object& &&>(obj) 이므로
+	// rvalue 캐스팅이 아닌 lvalue 캐스팅이 됩니다.
+	// 
+	// return static_cast<T&&>(obj);
+
+	// move 는 항상 rvalue 캐스팅 되어야 합니다.
+	//return static_cast<T&&>(obj); // T 안에 & 가 있을수 있습니다.
+	
+	return static_cast<std::remove_reference_t<T>&&>(obj);
 }
 
 int main()
