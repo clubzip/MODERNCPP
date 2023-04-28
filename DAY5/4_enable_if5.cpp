e4d1dadb6703a3d632a3298b105764dd86fe647f
ecourse <smkang@codenuri.co.kr>
2023-04-28 13:32:31 +0900

    Add files via upload

--- DAY5/4_enable_if5.cpp
+++ DAY5/4_enable_if5.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <type_traits>
 
-template<typename T>
-void printv(const T& v)
-{
+// 리턴 타입 자리에 "std::enable_if_t<조건, 반환타입>" 표기해보세요
 
-	if constexpr (std::is_pointer_v<T>)
-		std::cout << v << " : " << *v << std::endl;
-	else
-		std::cout << v << std::endl;
+template<typename T>
+
+void 
+
+printv(const T& v)
+{
+	std::cout << v << " : " << *v << std::endl;
 }
+
+template<typename T>
+
+void 
+
+printv(const T& v)
+{
+	std::cout << v << std::endl;
+}
+
 
 
 int main()
