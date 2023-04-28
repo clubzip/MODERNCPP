c68aecf2a4ebd2d34b89c26527724be19a6e70ad
ecourse <smkang@codenuri.co.kr>
2023-04-28 13:15:01 +0900

    Add files via upload

--- DAY5/4_enable_if3.cpp
+++ DAY5/4_enable_if3.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <type_traits>
 
-// std::enable_if<조건, 타입>::type 의 의미를 생각해 보세요
-// 조건 true  : 
-// 조건 false : 
+// std::enable_if<조건, T>::type 의 의미를 생각해 보세요
+// 조건 true  : "::type" 은 T
+// 조건 false : "::type" 이 없다
 
 template<typename T> 
 
-std::enable_if< std::is_integral_v<T>, T>::type
+typename std::enable_if< std::is_integral_v<T>, T>::type
 
 gcd(T a, T b)
 {
