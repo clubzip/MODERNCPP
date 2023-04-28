28d74c9c3aebd56710471bb794ea27ac2b660527
ecourse <smkang@codenuri.co.kr>
2023-04-28 10:43:44 +0900

    Add files via upload

--- DAY5/2_type_traits7.cpp
+++ DAY5/2_type_traits7.cpp
@@ -9,9 +9,10 @@ public:
 };
 
 // std::move 원리(구현)
-
+// T&  : lvalue 받겠다는 것
+// T&& : lvalue, rvalue 를 모두 받겠다는것
 template<typename T>
-T&& mymove(T& obj)
+T&& mymove(T&& obj)
 {
 	return static_cast<T&&>(obj);
 }
@@ -21,7 +22,8 @@ int main()
 	Object o1;
 	Object o2 = o1;				// copy
 	Object o3 = std::move(o2);	// move
-	Object o4 = mymove(o2);		// move
+	Object o5 = mymove(o3);		// move
+	Object o6 = mymove(Object()); // 안되야 할까요? 되야할까요?
 }
 
 
