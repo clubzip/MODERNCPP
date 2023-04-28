92c40f1462bda7881acce4edf98d1c452947c8df
ecourse <smkang@codenuri.co.kr>
2023-04-28 13:17:43 +0900

    Add files via upload

--- DAY5/4_enable_if3.cpp
+++ DAY5/4_enable_if3.cpp
@@ -5,12 +5,20 @@
 // 조건 true  : "::type" 은 T
 // 조건 false : "::type" 이 없다
 
+// 아래 코드는
+// T가 정수라면 "반환 타입은 T"
+// T가 정수 아니면 "반환 타입 만들기 실패" 
+//  => SFINAE 에 따라 에러가 아니라 후보에서 제외
+
+// 즉, T가 정수일때만 템플릿을 사용하겠다는 의미의 코드
+
 template<typename T> 
 
 typename std::enable_if< std::is_integral_v<T>, T>::type
 
 gcd(T a, T b)
 {
+	std::cout << "T" << std::endl;
 	return 0; .
 }
 
